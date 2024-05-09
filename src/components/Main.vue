f532efb32c4f7193f4f4a122da1ea7b6b066778a
ClaudioScordari <scordari22@gmail.com>
2024-05-09 20:04:37 +0200

    Rifatto stile

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,12 +5,93 @@ export default {
         return {
             questions: [
                 {
-                    question: "Qual è il fiume più lungo d'Italia?",
+                    question: "Qual è la capitale del Giappone?",
                     answers: [
-                        "A: Po",
-                        "B: Ticino",
-                        "C: Tevere",
-                        "D: Adige"
+                        "A: Tokyo",
+                        "B: Pechino",
+                        "C: Seul",
+                        "D: Bangkok"
+                    ]
+                },
+                {
+                    question: "Quale è il fiume più lungo del mondo?",
+                    answers: [
+                        "A: Nilo",
+                        "B: Amazonas",
+                        "C: Mississippi",
+                        "D: Yangtze"
+                    ]
+                },
+                {
+                    question: "In quale anno è stata firmata la Dichiarazione di Indipendenza degli Stati Uniti?",
+                    answers: [
+                        "A: 1776",
+                        "B: 1789",
+                        "C: 1812",
+                        "D: 1865"
+                    ]
+                },
+                {
+                    question: "Chi era il primo imperatore romano?",
+                    answers: [
+                        "A: Augusto",
+                        "B: Cesare",
+                        "C: Nerone",
+                        "D: Tiberio"
+                    ]
+                },
+                {
+                    question: "Qual è l'elemento chimico con simbolo H?",
+                    answers: [
+                        "A: Ossigeno",
+                        "B: Idrogeno",
+                        "C: Carbonio",
+                        "D: Azoto"
+                    ]
+                },
+                {
+                    question: "Chi ha scoperto la legge di gravitazione universale?",
+                    answers: [
+                        "A: Galileo Galilei",
+                        "B: Isaac Newton",
+                        "C: Albert Einstein",
+                        "D: Charles Darwin"
+                    ]
+                },
+                {
+                    question: "Qual è il quadro più famoso di Leonardo da Vinci?",
+                    answers: [
+                        "A: La Gioconda",
+                        "B: L'Ultima Cena",
+                        "C: Venere di Urbino",
+                        "D: La Primavera"
+                    ]
+                },
+                {
+                    question: "Chi è l'autore del romanzo 'Il Piccolo Principe'?",
+                    answers: [
+                        "A: Antoine de Saint-Exupéry",
+                        "B: Victor Hugo",
+                        "C: Marcel Proust",
+                        "D: Ernest Hemingway"
+                    ]
+                },
+                {
+                    question: "Chi è il primo ministro del Regno Unito?",
+                    answers: [
+                        "A: Boris Johnson",
+                        "B: Angela Merkel",
+                        "C: Emmanuel Macron",
+                        "D: Justin Trudeau"
+                    ]
+                },
+                {
+                    question: "Quale presidente degli Stati Uniti ha servito per quattro mandati?",
+                    answers: [
+                        "A: Franklin D. Roosevelt",
+                        "B: Abraham Lincoln",
+                        "C: George Washington",
+                        "D: Thomas Jefferson"
                     ]
                 }
             ]
@@ -23,70 +104,48 @@ export default {
 
 <template>
     <div id="main-template" class="text-light">
-        <div class="bg-up"></div>
-
         <div class="my-box text-center fw-bold">
-            <img class="mb-5" src="https://i.pinimg.com/474x/80/f3/e7/80f3e71a6ce9ab46b027423d056cef94.jpg">
+            <div class="bg-up py-5">
+                <img src="https://i.pinimg.com/474x/80/f3/e7/80f3e71a6ce9ab46b027423d056cef94.jpg">
+            </div>
 
-            <!-- Question -->
-            <p class="borders fs-4 mt-5 p-4">Domanda numero 1</p>
-
-            <!-- Answers -->
-            <div class="row">
-                <div class="col-md-6 col-sm-12">
-                    <div class="borders m-2">    
-                        1
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-sm-12">
-                    <div class="borders m-2">    
-                        2
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-sm-12">
-                    <div class="borders m-2">    
-                        3
-                    </div>
-                </div>
-
-                <div class="col-md-6 col-sm-12">
-                    <div class="borders m-2">    
-                        4
+            <div class="bg-down py-5" v-for="question in questions">
+                <div class='content container'>
+                    <!-- Question -->
+                    <p class="borders fs-4 p-3">{{ question.question }}</p>
+        
+                    <!-- Answers -->
+                    <div class="row">
+                        <div v-for="answer in question.answers" class="col-md-6 col-sm-12">
+                            <div class="my-card borders p-2 m-2">    
+                                {{ answer }}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        
-        <div class="bg-down"></div>
     </div>
 </template>
 
 <!-- --------------------------------------------------------------------------------- -->
 
 <style scoped>
-    #main-template {
-        height: 100vh;
-        position: relative;
+
+    /* ---------------------------- */
+
+    .my-card:hover{
+        background-color: lightgray;
+        cursor: pointer;
+        color: black;
     }
 
     .bg-up {
-        height: 45%;
-        background-color: #181989;
+        background-color: #11138C;
     }
 
     .bg-down {
-        height: 55%;
         background-color: #11093A;
-    }
-
-    .my-box {
-        position: absolute;
-        width: 70%;
-        min-width: 500px;
-        top: 5%;
-        left: 15%;
     }
 
     .borders{
