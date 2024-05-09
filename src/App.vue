1fe7b4cffed213dc824b4f8743ca9751db6473e3
Claudio Scordari <147527371+ClaudioScordari@users.noreply.github.com>
2024-05-09 16:38:56 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<!-- 
+    Come importare un componente:
+    1. si importa questo nel file App.vue nella prima riga dello script;
+    2. si dichiara il componente nella chiave components;
+    3. utilizzo del componente tramite un self closing tag con il nome del componente.
+-->
+
+<script>
+// 1. import AppQualcosa from './components/AppQualcosa.vue';
+
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    // 2. Dichiarazione dei componenti
+    components: {
+        // AppQualcosa
+    }
+}
+
+
+</script>
+
+<!-- --------------------------------------------------------------------------------- -->
+
+<template>
+    <div>
+        <!-- 3. <AppQualcosa/> -->
+
+
+    </div>
+</template>
+
+<!-- --------------------------------------------------------------------------------- -->
+
+<style scoped>
+
+</style>
